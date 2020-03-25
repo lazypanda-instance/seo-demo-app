869a28f007ead7b837e2915f9c18b13e4abac473
Das, Sudipta C <X2SCDAS@southernco.com>
2020-03-25 16:05:08 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'demo-seo';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+
+  }
+
+  ngOnInit() {
+    window.addEventListener('orientationchange', this.handleOrientationChange); // Orientation change event
+
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('App_name', 'Seo-Demo');
+
+      setTimeout(() => {
+        console.log('App Name: ', localStorage.getItem('App_name'));
+      }, 2000);
+    }
+
+  }
+
+  handleOrientationChange = (event) => {
+    console.log('Orientation changed: ', event); // Print in console
+  }
+}
