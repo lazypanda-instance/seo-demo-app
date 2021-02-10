bb907b403affebf3c25134bb1eea339c76c86f86
alejandropr27@nauta.cu <alejandropr27@nauta.cu>
2021-02-10 10:37:43 -0500

    'change style and main'

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'hammerjs';
 
 if (environment.production) {
   enableProdMode();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-});
